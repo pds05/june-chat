6fa5d8d71cae73655594ae4e402596ce3cab892a
Dmitry Petrov <166365976+pds05@users.noreply.github.com>
2024-06-26 20:33:35 +0400

    #june_chat_part2 lesson2 initial commit

--- server/src/main/java/ru/otus/june/chat/server/ClientHandler.java
+++ server/src/main/java/ru/otus/june/chat/server/ClientHandler.java
@@ -10,13 +10,14 @@ public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
-
     private String username;
-
-    private static int usersCount = 0;
 
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public ClientHandler(Server server, Socket socket) throws IOException {
@@ -24,11 +25,39 @@ public class ClientHandler {
         this.socket = socket;
         this.in = new DataInputStream(socket.getInputStream());
         this.out = new DataOutputStream(socket.getOutputStream());
-        usersCount++;
-        this.username = "user" + usersCount;
         new Thread(() -> {
             try {
                 System.out.println("Подключился новый клиент");
+                while (true) {
+                    String message = in.readUTF();
+                    if (message.equals("/exit")) {
+                        sendMessage("/exitok");
+                        return;
+                    }
+                    if (message.startsWith("/auth ")) {
+                        String[] elements = message.split(" ");
+                        if (elements.length != 3) {
+                            sendMessage("Неверный формат команды /auth");
+                            continue;
+                        }
+                        if (server.getAuthenticationProvider().authenticate(this, elements[1], elements[2])) {
+                            break;
+                        }
+                        continue;
+                    }
+                    if (message.startsWith("/register ")) {
+                        String[] elements = message.split(" ");
+                        if (elements.length != 4) {
+                            sendMessage("Неверный формат команды /register");
+                            continue;
+                        }
+                        if (server.getAuthenticationProvider().registration(this, elements[1], elements[2], elements[3])) {
+                            break;
+                        }
+                        continue;
+                    }
+                    sendMessage("Перед работой с чатом необходимо выполнить аутентификацию '/auth login password' или регистрацию '/register login password username'");
+                }
                 while (true) {
                     String message = in.readUTF();
                     if (message.startsWith("/")) {
